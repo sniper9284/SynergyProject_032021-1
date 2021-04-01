d848a339da7e9cf1e4f0e3bb3730e681023409db
sniper9284 <sniper-sochi@yandex.ru>
2021-04-01 22:08:30 +0300

    Минимальные изменения. Подготовка к ДЗ из дома.

--- src/java/ru/synergyitacademy/lesson2/Runner.java
+++ src/java/ru/synergyitacademy/lesson2/Runner.java
@@ -3,9 +3,9 @@ package ru.synergyitacademy.lesson2;
 public class Runner {
 
     public static void main(String[] args) {
-        int x = 5 - 2;
-        int y = 2 * x + 10;
-        int z = x * x + 5 * x + 28;
+        int x = 8 - 2;
+        int y = 3 * x + 10;
+        int z = x * x + 5 * x + 22;
         System.out.println("x: " + x + " y: " + y + " z: " + z);
     }
 }
