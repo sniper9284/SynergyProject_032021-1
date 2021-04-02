55c4b7cbc704ebdb8901c96bd3ac6aedeb69cb0b
sniper9284 <sniper-sochi@yandex.ru>
2021-04-02 11:37:09 +0300

    Решение 2 задачи. Добавлен запрос к пользователю.

--- src/java/ru/synergyitacademy/lesson2/Runner.java
+++ src/java/ru/synergyitacademy/lesson2/Runner.java
@@ -1,11 +1,20 @@
 package ru.synergyitacademy.lesson2;
 
+import java.util.Scanner;
+
 public class Runner {
 
     public static void main(String[] args) {
-        int x = 8 - 2;
-        int y = 3 * x + 10;
-        int z = x * x + 5 * x + 22;
-        System.out.println("x: " + x + " y: " + y + " z: " + z);
+
+        Scanner console = new Scanner(System.in);
+        //запрос чисел от пользователя без проверки вводимых данных.
+        System.out.println("Введите первое число и нажмите Enter.");
+        int x = console.nextInt();
+        System.out.println("Введите второе число и нажмите Enter.");
+        int y = console.nextInt();
+        // вычисления.
+        int z = x * y + x * y / 2;
+
+        System.out.println("Ответ = " + z);
     }
 }
